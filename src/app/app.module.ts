a38993d80acab17fc3809dbfc5813f4c4deab620
Sima Kundu <Sima Kundu@accionlabs.in>
2017-12-10 16:55:21 +0530

    component and routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalaryComponent } from './galary/galary.component';
 
-
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {path: 'galary',component: GalaryComponent},  
+  {path: 'product',component: ProductComponent},
+  {path: 'contact',component: ContactComponent},
+  { path: '',redirectTo: '/home',pathMatch: 'full'} 
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    ContactComponent,
+    GalaryComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
