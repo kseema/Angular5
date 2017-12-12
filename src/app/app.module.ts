e66008d141ea44517cc64515869ffdd30c4b4455
Sima Kundu <Sima Kundu@accionlabs.in>
2017-12-12 09:00:17 +0530

    angular material integreted with application(https://material.angular.io/components/checkbox/examples)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
 import { GalaryComponent } from './galary/galary.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'; 
+
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -25,7 +29,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,MatButtonModule, MatCheckboxModule,FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
